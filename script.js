4aa74bc1d03f1304f6945ad81d7d03cb7026971d
RyanGabriel19 <77082463+RyanGabriel19@users.noreply.github.com>
2024-11-08 18:38:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,179 @@
+function Resposta1(){
+    let res1 = document.querySelector("#resposta1").value
+
+    switch (res1.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso1").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso1").innerHTML="Certa resposta!"
+            break
+        case "c":
+            document.querySelector("#aviso1").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso1").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta2(){
+    let res2 = document.querySelector("#resposta2").value
+
+    switch (res2.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso2").innerHTML="Certa resposta!"
+            break
+        case "b":
+            document.querySelector("#aviso2").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso2").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso2").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta3(){
+    let res3 = document.querySelector("#resposta3").value
+
+    switch (res3.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso3").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso3").innerHTML="Certa resposta!"
+            break
+        case "c":
+            document.querySelector("#aviso3").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso3").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta4(){
+    let res4 = document.querySelector("#resposta4").value
+
+    switch (res4.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso4").innerHTML="Certa resposta!"
+            break
+        case "b":
+            document.querySelector("#aviso4").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso4").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso4").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta5(){
+    let res5 = document.querySelector("#resposta5").value
+
+    switch (res5.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso5").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso5").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso5").innerHTML="Certa resposta!"
+            break
+        default:
+            document.querySelector("#aviso5").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta6(){
+    let res6 = document.querySelector("#resposta6").value
+
+    switch (res6.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso6").innerHTML="Certa resposta!"
+            break
+        case "b":
+            document.querySelector("#aviso6").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso6").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso6").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta7(){
+    let res7 = document.querySelector("#resposta7").value
+
+    switch (res7.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso7").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso7").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso7").innerHTML="Certa resposta!"
+            break
+        default:
+            document.querySelector("#aviso7").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta8(){
+    let res8 = document.querySelector("#resposta8").value
+
+    switch (res8.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso8").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso8").innerHTML="Certa resposta!"
+            break
+        case "c":
+            document.querySelector("#aviso8").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso8").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta9(){
+    let res9 = document.querySelector("#resposta9").value
+
+    switch (res9.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso9").innerHTML="Certa resposta!"
+            break
+        case "b":
+            document.querySelector("#aviso9").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso9").innerHTML="Resposta errada."
+            break
+        default:
+            document.querySelector("#aviso9").innerHTML="Não temos essa alternativa."
+    }
+}
+
+function Resposta10(){
+    let res10 = document.querySelector("#resposta10").value
+
+    switch (res10.toLowerCase()){
+        case "a":
+            document.querySelector("#aviso10").innerHTML="Resposta errada."
+            break
+        case "b":
+            document.querySelector("#aviso10").innerHTML="Resposta errada."
+            break
+        case "c":
+            document.querySelector("#aviso10").innerHTML="Certa resposta!"
+            break
+        default:
+            document.querySelector("#aviso10").innerHTML="Não temos essa alternativa."
+    }
+}
